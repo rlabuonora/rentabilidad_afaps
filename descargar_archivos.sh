964e7c73e194dd3bba54c9714dcf60ae660c7466
Rafael La Buonora <info@rlabuonora.com>
2021-03-10 10:42:02 -0300

    Initial commit

---
+++ descargar_archivos.sh
@@ -0,0 +1,12 @@
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenrentnominal.xls --output ./data/rentnominal.xls
+
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenrentneta1.xls --output ./data/rentaneta1.xls
+
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenrentneta5.xls --output ./data/rentaneta5.xls
+
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenrentreal.xls --output ./data/rentareal.xls
+
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenvalorcuotafindemes.xls --output ./data/valorcuotafindemes.xls
+
+curl https://www.bcu.gub.uy/Servicios-Financieros-SSF/AFAPSeries/afesenvalorcuotapromediomes.xls --output ./data/valorcuotapromediomes.xls
+
