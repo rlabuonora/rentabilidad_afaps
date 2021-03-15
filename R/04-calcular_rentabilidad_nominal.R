673da3cd5c1aff6f9d7e40d16cf72c33c74d4f89
Rafael La Buonora <info@rlabuonora.com>
2021-03-15 14:19:25 -0300

    Compara rentabilidad nominal

--- R/04-calcular_rentabilidad_nominal.R
+++ R/04-calcular_rentabilidad_nominal.R
@@ -20,6 +20,41 @@ rentabilidad_nominal_cess <- valor_cuota_total %>%
   ungroup() %>% 
   mutate(fuente="cess")
 
+rentabilidad_nominal_bcu <- readRDS(here::here("data", "rds", "rentabilidad_nominal_bcu.rds")) %>% 
+  filter(administradora %in% c("afap_sura", "integracion",
+                               "republica", "union_capital")) %>% 
+  filter(fondo=="Acumulación") %>% 
+  rename(rentabilidad_nominal_bcu=rentabilidad_nominal) %>% 
+  select(ano_mes, rentabilidad_nominal_bcu, administradora)
+
+rentabilidad_nominal_comparada <- rentabilidad_nominal_cess %>% 
+  filter(fondo=="Acumulación") %>% 
+  left_join(rentabilidad_nominal_bcu, by=c("administradora", "ano_mes"))
+
+sura <- rentabilidad_nominal_comparada %>% 
+  # filter(administradora %in% c("afap_sura", "integracion",
+  #                              "republica", "union_capital")) %>% 
+  filter(administradora=="afap_sura")
+
+integracion <- rentabilidad_nominal_comparada %>% 
+  # filter(administradora %in% c("afap_sura", "integracion",
+  #                              "republica", "union_capital")) %>% 
+  filter(administradora=="integracion")
+
+republica <- rentabilidad_nominal_comparada %>% 
+  # filter(administradora %in% c("afap_sura", "integracion",
+  #                              "republica", "union_capital")) %>% 
+  filter(administradora=="republica")
+
+union_capital <- rentabilidad_nominal_comparada %>% 
+  # filter(administradora %in% c("afap_sura", "integracion",
+  #                              "republica", "union_capital")) %>% 
+  filter(administradora=="union_capital")
+
+
+WriteXLS(c("sura", "integracion", "republica", "union_capital"), 
+         ExcelFileName = here::here("output", "VerificarRentabilidadNominal.xlsx"))
+
 
 saveRDS(rentabilidad_nominal_cess, 
         file = here::here("data", "rds", "rentabilidad_nominal_cess.rds"))
